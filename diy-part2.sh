1a068789217e81be8c11095d710727d08566aa68
maximaqiu <34705088+maximaqiu@users.noreply.github.com>
2021-11-30 13:20:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/M_wlan_\$\{devidx\}/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/M_wlan_${devidx}/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
