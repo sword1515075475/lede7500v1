64721507fcba24bd809e31438eeec21bb06c5854
maximaqiu <34705088+maximaqiu@users.noreply.github.com>
2022-06-09 12:51:52 +0800

    Update diy-part2.sh
    更新r22.5.5

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,5 +14,5 @@
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # 修改版本信息
-sed -i "s/DISTRIB_REVISION='R21.12.1'/DISTRIB_REVISION='R21.12.9 by Max'/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/DISTRIB_REVISION='R22.5.5'/DISTRIB_REVISION='R22.5.5 by Max'/g" package/lean/default-settings/files/zzz-default-settings
 sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='Linksys EA8500 Router '/g" package/lean/default-settings/files/zzz-default-settings
